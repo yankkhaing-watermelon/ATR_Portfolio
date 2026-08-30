0e0545b9e746d5d8a88533dc40b9638257fe4cf0
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 18:58:30 +0800

    Refresh Android overflow fix cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "atr-portfolio-v12";
+const CACHE = "atr-portfolio-v13";
 const SHELL = ["/", "/index.html", "/styles.css", "/mobile.css", "/pwa.js", "/app-v4.js", "/manage.html", "/manage.css", "/manage.js", "/risk.html", "/risk.js", "/signals.html", "/signals.js", "/chart.html", "/chart.js", "/analytics.html", "/analytics.js", "/decision.html", "/decision.js", "/daily.html", "/daily.js", "/reviews.html", "/reviews.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(()=>self.clients.claim())));
