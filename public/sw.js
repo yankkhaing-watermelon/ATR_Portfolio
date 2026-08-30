8eee147ae46557028f5eb3f9ca9764bd00dcfe2d
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 16:23:24 +0800

    Cache Phase 5 signal console

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "atr-portfolio-v6";
-const SHELL = ["/", "/index.html", "/styles.css", "/app-v4.js", "/manage.html", "/manage.css", "/manage.js", "/risk.html", "/risk.js", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "atr-portfolio-v7";
+const SHELL = ["/", "/index.html", "/styles.css", "/app-v4.js", "/manage.html", "/manage.css", "/manage.js", "/risk.html", "/risk.js", "/signals.html", "/signals.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch", event => {
