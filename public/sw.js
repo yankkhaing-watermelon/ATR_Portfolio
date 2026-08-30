b1cf25be30e77909b5e0e046dbec741a84529983
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 13:38:32 +0800

    Phase 2: update public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "atr-portfolio-v1";
+const CACHE = "atr-portfolio-v2";
 const SHELL = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
