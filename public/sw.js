3a0892caa9f0d24a3f37c294e49226c82d2d6023
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 12:50:06 +0800

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE = "atr-portfolio-v1";
+const SHELL = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match("/index.html"))));
+});
