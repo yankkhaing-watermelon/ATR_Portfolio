81473fe8ac25758d205d83615aa8731a5c570fad
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 14:06:34 +0800

    Phase 2: refresh PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE = "atr-portfolio-v2";
+const CACHE = "atr-portfolio-v3";
 const SHELL = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(fetch(event.request).then(response => {
